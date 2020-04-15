6c5db4d04d0014e49efb8a196be904e0fe9b53b0
Ravneet Kaur <ravneet.kaur01@nagarro.com>
2020-04-15 21:56:34 +0530

    Made UI more user-friendly

--- public/scripts/indexScript.js
+++ public/scripts/indexScript.js
@@ -35,36 +35,72 @@ async function postNewTask(theForm)
     
 //     this.loadTasks();
 // }
-function generateTaskHtml(task,index){
-    return `
-    <tr>
-        <td>${task.id}</td>
-        <td>${task.title}</td>
-        <td>${task.dueDate}</td>
-    <tr>
-    `
+async function getSelectedTask(id)
+{
+    const resp = await fetch('/tasks/'+id,{
+        method : 'GET',
+        headers :{
+            'Content-Type' : 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    const task = await resp.json()
+    return task
+
+}
+async function getNotesOfSelectedTask(id)
+{
+    const resp = await fetch('/tasks/'+id+'/notes',{
+        method : 'GET',
+        headers :{
+            'Content-Type' : 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    const notes = await resp.json()
+    return notes
+
+}
+async function onTaskClick(id)
+{
+    var task = await getSelectedTask(id)
+    console.log(task)
+    var notes = await getNotesOfSelectedTask(id)
     
 }
+function generateTaskHtml(task){
+    return `
+    <div class="card text-white" id="task-card">
+        <div class="card-header" onclick="onTaskClick(${task.id})">
+            <div class="row">
+                <div class ="col-md-1" id="taskId">
+                    ${task.id}
 
-
+                </div>
+                <div class ="col-md-7">
+                    ${task.title}
+                </div>
+                <div class ="col-md-4">
+                    ${task.dueDate}
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    `    
+}
 async function loadTasks(){
     
     var tasks = await getTasks()
     console.log(tasks)
     
     // let taskHtml = tasks.reduce ( (html,task,index) => html+=this.generateTaskHtml(task,index),'')
-    let taskHtml =''
-    // var index=0
-    // for(let task in tasks)
-    // {
-    //     taskHtml += generateTaskHtml(task[index])
-    //     index++
-    // }
+    let taskHtml =''   
     for (var i = 0; i < tasks.length; i++) {
         var task = tasks[i];
         taskHtml += generateTaskHtml(task)
     }
-    document.getElementById("table-body").innerHTML = taskHtml;
+    document.getElementById("all-tasks-card-body").innerHTML = taskHtml;
 }
 
 loadTasks()
--- routes/tasks.js
+++ routes/tasks.js
@@ -8,6 +8,25 @@ route.get('/',async(req , res) =>{
     res.send(tasks)
 })
 
+route.get('/:id',async(req,res) =>{
+
+    if (isNaN(Number(req.params.id))) {
+        return res.status(400).send({
+          error: 'task id must be an integer',
+        })
+    }
+    const task = await Tasks.findByPk(req.params.id)
+
+    if (!task) {
+        return res.status(404).send({
+        error: 'No task found with id = ' + req.params.id,
+        })
+    }
+    res.send(task)
+
+
+})
+
 route.post('/',async(req,res) =>{
     const newTask = await Tasks.create({
         title : req.body.title,
